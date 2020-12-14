d846e1d68b765a486cdc2867eca425947e1fa89e
swodig 112 <swodig@gmail.com>
2020-12-14 20:41:35 +0330

    Add random() to UniformDistribution
    Add "random" function to UniformDistribution in classes/distributions.py

--- classes/distributions.py
+++ classes/distributions.py
@@ -2,8 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 
+
 class UniformDistribution:
     def __init__(self, low=0, high=1):
         self.low = low
         self.high = high
+
+
+    def random(self, size=None):
+        return np.random.uniform(self.low, self.high, size)
 
