359b7b93e56ebf365f0580c79f9ca79bc34493c4
swodig 112 <swodig@gmail.com>
2020-12-14 20:29:18 +0330

    Add UniformDistribution class
    Add __init__

--- classes/distributions.py
+++ classes/distributions.py
@@ -1,3 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
+
+class UniformDistribution:
+    def __init__(self, low=0, high=1):
+        self.low = low
+        self.high = high
+
