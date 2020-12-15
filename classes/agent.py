7e9eb8ec703c4af0176a1f8c9e9a6ce7f3064656
swodig 112 <swodig@gmail.com>
2020-12-15 19:23:05 +0330

    Add NormalDistribution
    Add Agent class to classes/agent.py. Also add the __init__ function (to
    the class)

--- classes/agent.py
+++ classes/agent.py
@@ -1,3 +1,25 @@
 import numpy as np
 from distributions import NormalDistribution
 
+
+class Agent :
+    def __init__ (self, name, money, income, expenses, income_changes,
+            expenses_changes, hiring_possibility, firing_possibility, age=216):
+        self.name = name
+        self.age = age
+        self.money = money
+        self.income = income
+        self.expenses = expenses
+        self.working = False
+        if self.income != 0:
+            self.working = True
+        self.hiring_possibility = NormalDistribution(hiring_possibility[0],
+                hiring_possibility[1])
+        self.firing_possibility = NormalDistribution(firing_possibility[0],
+                firing_possibility[1])
+        self.income_changes = NormalDistribution(income_changes[0],
+                income_changes[1])
+        self.expenses_changes = NormalDistribution(expenses_changes[0],
+                expenses_changes[1])
+        self.previncome = self.income * 0.9
+
