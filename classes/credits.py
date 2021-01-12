e21909c4db09168c1f14ec68ba53a9dfc1aa9d45
swodig 112 <swodig@gmail.com>
2021-01-12 15:20:47 +0330

    Add add_debt
    Add add_debt function to Debt class in classes/credits.py

--- classes/credits.py
+++ classes/credits.py
@@ -3,3 +3,9 @@ class DebtManager:
         self.debtID = 0
         self.debts = {}
 
+
+    def add_debt(self, debt_amount, birth_month, paid=0):
+        self.debts[self.debtID] = {"debt_amount": debt_amount, "birth_month":
+                birth_month, "paid": paid}
+        self.debtID += 1
+
