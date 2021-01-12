fbee15023b7eb2173c1ca417bb03de55b8317bc1
swodig 112 <swodig@gmail.com>
2021-01-12 11:51:53 +0330

    Edit __init__ and update to for plotting support
    Edit Agent.__init__ function and Agent.update function in
    classes/agent.py to add plotting support

--- classes/agent.py
+++ classes/agent.py
@@ -8,10 +8,12 @@ class Agent :
             expenses_changes, hiring_possibility, firing_possibility, age=216):
         self.name = name
         self.age = age
+        self.initial_age = age
         self.money = money
         self.income = income
         self.expenses = expenses
         self.working = False
+        self.results = []
         if self.income != 0:
             self.working = True
         self.hiring_possibility = NormalDistribution(hiring_possibility[0],
@@ -30,12 +32,17 @@ class Agent :
         if not self.working:
             if self.hiring_possibility.random() >= 1:
                 self.working = True
+                self.hiring_possibility.mu += 0.05
                 self.income = self.previncome * 0.9
                 self.previncome = self.income
         elif self.firing_possibility.random() >= 1:
             self.income = 0
             self.working = False
+            self.firing_possibility.mu += 0.05
         self.money += self.income - self.expenses
         self.income *= self.income_changes.random()
         self.expenses *= self.expenses_changes.random()
+
+        self.add_results(self.age, self.money, self.income, self.expenses,
+                self.working)
 
